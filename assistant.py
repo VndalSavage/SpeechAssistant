6033e208edc325278d64c687ab84f09b79845ae7
VndalSavage <66523724+VndalSavage@users.noreply.github.com>
2021-05-06 11:23:58 -0400

    Create assistant.py

---
+++ assistant.py
@@ -0,0 +1,115 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+chromedir= "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("My name is Felicia. How may I help you?")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        speak("Listening")
+        r.pause_threshold = 0.5
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")   
+        speak("Recognizing") 
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception:
+        # print(e)    
+        print("Say that again please...")  
+        speak("Sorry, I didn't quite get that. Please say it again")
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    #while True:
+    if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+ 
+        elif 'open youtube' in query:
+            webbrowser.get(chromedir).open("https://youtube.com")
+            # or use webbrowser.open("youtube.com") for internet explorer 
+
+        elif 'open google' in query:
+            webbrowser.get(chromedir).open("https://www.google.com/")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.get(chromedir).open("https://stackoverflow.com")   
+
+        elif 'play music' in query:
+            webbrowser.get(chromedir).open("https://open.spotify.com/collection/playlists")
+            
+            #music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+            #songs = os.listdir(music_dir)
+            #print(songs)    
+            #os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\Manoj\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'email to aryan' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "aru.18072002@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry mate, couldn't send that shit") 
